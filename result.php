050d486c5b560351faad0df084fb4aed774d62d8
chiachun wu <a0283137@gmail.com>
2022-05-15 02:53:00 +0800

    增加判斷式

--- result.php
+++ result.php
@@ -15,7 +15,31 @@
         echo "BMI為" . $bmi;
         ?>
     </h1>
-
+    <div>
+        <?php
+        // $result = $bmi;
+        switch($bmi){
+            case $bmi < 18.5;
+            echo "體重過輕";
+            break;
+            case $bmi >18.5 && $bmi<24;
+            echo "體重正常";
+            break;
+            case $bmi >=24 && $bmi<27;
+            echo "體重過重";
+            break;
+            case $bmi >=27 && $bmi<30;
+            echo "輕度肥胖";
+            break;
+            case $bmi >=30 && $bmi<35;
+            echo "中度肥胖";
+            break;
+            default;
+            echo "重度肥胖";
+            break;
+        }
+        ?>
+    </div>
     <a href="./BMI.html">
         <button>
             回上一頁
